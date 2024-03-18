9d22e89189b4abb78ad2cd6a7974292829846296
soumyasis king <92395483+Venturesum1@users.noreply.github.com>
2024-03-18 11:37:59 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+const generateResponse = (data) => {
+    const response = {
+        "is_success": true,
+        "user_id": "john_doe_17091999",
+        "email": "john@xyz.com",
+        "roll_number": "ABCD123",
+        "odd_numbers": data.filter(num => num % 2 !== 0 && !isNaN(num)),
+        "even_numbers": data.filter(num => num % 2 === 0 && !isNaN(num)),
+        "alphabets": data.filter(item => typeof item === 'string' && item.match(/^[A-Za-z]+$/)).map(item => item.toUpperCase())
+    };
+
+    return response;
+};
+
+app.post('/bfhl', (req, res) => {
+    console.log('Data received:', req.body.data);
+    const data = req.body.data;
+
+    const response = generateResponse(data);
+
+    res.json(response);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
